a45ceaf6350d959a78cbf1e2b3bb4c811b5eca21
jcncs <jcncs1@hotmail.com>
2021-10-29 03:29:55 +0800

    Create Dashboard

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,26 +1,40 @@
 ﻿using FWMS.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
+using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Net;
 using System.Linq;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace FWMS.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IConfiguration _configuration;
         private readonly ILogger<DashboardController> _logger;
 
-        public DashboardController(ILogger<DashboardController> logger)
+        public DashboardController(ILogger<DashboardController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            return View();
+            
+            List<ViewDashboardModel> result = Deserialize<List<ViewDashboardModel>>(RetrieveData("GET", "Donation:GET:ViewAvailableDonations"));
+            return View(result);
+        }
+
+        public IActionResult CancelDonation()
+        {
+
+            List<ViewDashboardModel> result = Deserialize<List<ViewDashboardModel>>(RetrieveData("PUT", "Donation:PUT:CancelDonation"));
+            return View(result);
         }
 
         public IActionResult Privacy()
@@ -32,6 +46,30 @@ namespace FWMS.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public static T Deserialize<T>(string jsonData)
+        {
+            JsonSerializer json = new JsonSerializer();
+            return json.Deserialize<T>(new JsonTextReader(new StringReader(jsonData)));
+        }
+
+        public string RetrieveData(string requestMethod, string invoke)
+        {
+            string response = string.Empty;
+            var apiGateway = _configuration["ApiGateway"];
+            var invokePath = _configuration[invoke];
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(apiGateway + invokePath);
+            httpWebRequest.ContentType = "application/json; charset=utf-8";
+            httpWebRequest.Method = requestMethod;
+            httpWebRequest.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+            httpResponse.Close();
+            return response;
         }
     }
 }
---
+++ Models/ViewDashboardModel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace FWMS.Models
+{
+    public class ViewDashboardModel
+    {
+        public string donationId { get; set; }
+        public string donationName { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+        public int quantity { get; set; }
+        public DateTime expiryDate { get; set; }
+        public string locationName { get; set; }
+        public DateTime createdDate { get; set; }
+        public string createdBy { get; set; }
+        public DateTime updatedDate { get; set; }
+        public string updatedBy { get; set; }
+        public string userId { get; set; }
+        public string reservedBy { get; set; }
+        public DateTime? reservedDate { get; set; }
+        public string collectionId { get; set; }
+        public string foodEntryId { get; set; }
+    }
+}
